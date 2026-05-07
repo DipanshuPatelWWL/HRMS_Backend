950b72aca1a20cf2b5d6dc3f64dd4b44966216a3
jahidvicky <jahidvicky@gmail.com>
2026-05-07 10:42:42 +0530

    fix v3

--- src/app.js
+++ src/app.js
@@ -43,8 +43,9 @@ app.use("/uploads", express.static("uploads"));
 const io = new Server(server, {
     cors: {
         origin: [
-            "http://localhost:5173",
-            "http://localhost:5174",
+            // "http://localhost:5173",
+            // "http://localhost:5174",
+            "https://wwl-hrms.vercel.app"
         ],
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
         credentials: true,
