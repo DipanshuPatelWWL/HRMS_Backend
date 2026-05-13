4b349df1f29a3621121039c4db047ccc3acaa0ff
jahidvicky <jahidvicky@gmail.com>
2026-05-13 12:49:58 +0530

    change url

--- src/app.js
+++ src/app.js
@@ -46,9 +46,9 @@ app.use("/uploads", express.static("uploads"));
 const io = new Server(server, {
     cors: {
         origin: [
-            "http://localhost:5173",
-            "http://localhost:5174",
-            // "https://wwl-hrms.vercel.app/"
+            // "http://localhost:5173",
+            // "http://localhost:5174",
+            "https://wwl-hrms.vercel.app/"
         ],
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
         credentials: true,
