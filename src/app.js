48a67047d682024cea3591cb63ba96c06afab2f5
jahidvicky <jahidvicky@gmail.com>
2026-05-27 14:26:59 +0530

    fix capture

--- src/app.js
+++ src/app.js
@@ -66,6 +66,10 @@ app.use(cors({
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use((req, res, next) => {
+    res.setHeader("Connection", "keep-alive");
+    next();
+});
 app.use("/uploads", express.static("uploads"));
 
 const io = new Server(server, {
